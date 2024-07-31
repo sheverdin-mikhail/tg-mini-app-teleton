26d85821f92aecfbcb1f5a125796623acbbd7e0c
Mikhail <sheverdin-mikhail@gmail.com>
2024-08-01 03:27:14 +0400

    add close btn for up level conditions modal

--- src/features/UpLevelConditions/ui/UpLevelConditionsModal/UpLevelConditionsModal.tsx
+++ src/features/UpLevelConditions/ui/UpLevelConditionsModal/UpLevelConditionsModal.tsx
@@ -3,22 +3,34 @@ import clsx from 'clsx';
 import { ModalHeader } from '@telegram-apps/telegram-ui/dist/components/Overlays/Modal/components/ModalHeader/ModalHeader';
 import cls from './UpLevelConditionsModal.module.scss';
 import { UpLevelConditionsList } from '../UpLevelConditionsList/UpLevelConditionsList';
+import { ModalClose } from '@telegram-apps/telegram-ui/dist/components/Overlays/Modal/components/ModalClose/ModalClose';
+import { AwesomeIcon } from '@/shared/ui/AwesomeIcon/AwesomeIcon';
 
 interface UpLevelConditionsModalProps {
   className?: string;
   trigger: any;
-  // isOpen?: boolean;
-  // setIsOpen: () => void;
+  isOpen?: boolean;
+  setIsOpen: (isOpen: boolean) => void;
 }
 
 export const UpLevelConditionsModal: React.FC<UpLevelConditionsModalProps> = (props) => {
-  const { className, trigger } = props;
+  const { className, trigger, setIsOpen, isOpen = false } = props;
+
+  const onCloseHandler = () => {
+    setIsOpen(false);
+  }
 
   return (
     <Modal
+      dismissible={false}
+      open={isOpen}
       className={clsx(cls.upLevelConditionsModal, {}, [className])}
-      // open={isOpen}
-      header={<ModalHeader />}
+      header={<ModalHeader 
+        after={<div onClick={onCloseHandler}>
+          <ModalClose><AwesomeIcon logo='fa-solid fa-xmark' /></ModalClose>
+        </div>}
+      />}
+      
       trigger={trigger}
     >
       <UpLevelConditionsList />
--- src/widgets/Header/ui/Header/Header.tsx
+++ src/widgets/Header/ui/Header/Header.tsx
@@ -5,7 +5,7 @@ import {
 } from '@telegram-apps/telegram-ui';
 import { GameLevelProgress } from '@/features/Game';
 import { UpLevelConditionsModal } from '@/features/UpLevelConditions';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import AvatarImage from '@/shared/assets/img/logo.jpg';
 import cls from './Header.module.scss';
 
@@ -17,6 +17,8 @@ export const Header: React.FC<HeaderProps> = (props) => {
   const { className } = props;
   const { tgUser } = useTelegram();
   const trigger = useRef(null);
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
   return (
     <>
       <header className={clsx(cls.header, {}, [className])}>
@@ -24,14 +26,17 @@ export const Header: React.FC<HeaderProps> = (props) => {
           <Avatar src={tgUser?.photoUrl ?? AvatarImage} className={cls.avatar} />
           <div className={clsx(cls.userLabel, cls.col)}>
             <Caption weight="2" caps>Streamers Producer</Caption>
-            <Text weight="2" caps className={cls.name}>{tgUser?.firstName} qdqwd qwd </Text>
+            <Text weight="2" caps className={cls.name}>{tgUser?.firstName}</Text>
           </div>
         </div>
-        <UpLevelConditionsModal trigger={(
-          <div className={cls.block} ref={trigger}>
-            <GameLevelProgress />
-          </div>
-        )}
+        <UpLevelConditionsModal 
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          trigger={(
+            <div className={cls.block} ref={trigger}>
+              <GameLevelProgress onClick={() => setIsOpen(true)} />
+            </div>
+          )}
         />
       </header>
     </>
