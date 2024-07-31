f35b734d4a555877a2bfa2adb14c060ca148a485
Mikhail <sheverdin-mikhail@gmail.com>
2024-08-01 01:41:26 +0400

    fix views icon

--- src/shared/lib/hooks/useTelegram/useTelegram.ts
+++ src/shared/lib/hooks/useTelegram/useTelegram.ts
@@ -1,7 +1,7 @@
 import {
   initSettingsButton,
-  // mockTelegramEnv,
-  // parseInitData,
+  mockTelegramEnv,
+  parseInitData,
   retrieveLaunchParams,
 } from '@telegram-apps/sdk';
 import { TelegramWebApps } from 'app/types/global';
@@ -17,29 +17,29 @@ declare global {
 
 // убрать на проде
 // eslint-disable-next-line max-len
-// const initDataRaw = 'query_id=AAHWD2IuAAAAANYPYi4t4HOn&user=%7B%22id%22%3A778178518%2C%22first_name%22%3A%22%D0%9C%D0%B8%D1%85%D0%B0%D0%B8%D0%BB%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22sheverdin_mikhail%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1722422559&hash=5683f97303316162de127b1065c782304786b681a469eb98a48d2465ef398f03';
+const initDataRaw = 'query_id=AAHWD2IuAAAAANYPYi4t4HOn&user=%7B%22id%22%3A778178518%2C%22first_name%22%3A%22%D0%9C%D0%B8%D1%85%D0%B0%D0%B8%D0%BB%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22sheverdin_mikhail%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1722422559&hash=5683f97303316162de127b1065c782304786b681a469eb98a48d2465ef398f03';
 
-// mockTelegramEnv({
-//   themeParams: {
-//     accentTextColor: '#6ab2f2',
-//     bgColor: '#17212b',
-//     buttonColor: '#5288c1',
-//     buttonTextColor: '#ffffff',
-//     destructiveTextColor: '#ec3942',
-//     headerBgColor: '#17212b',
-//     hintColor: '#708499',
-//     linkColor: '#6ab3f3',
-//     secondaryBgColor: '#232e3c',
-//     sectionBgColor: '#17212b',
-//     sectionHeaderTextColor: '#6ab3f3',
-//     subtitleTextColor: '#708499',
-//     textColor: '#f5f5f5',
-//   },
-//   initData: parseInitData(initDataRaw),
-//   initDataRaw,
-//   version: '7.2',
-//   platform: 'tdesktop',
-// });
+mockTelegramEnv({
+  themeParams: {
+    accentTextColor: '#6ab2f2',
+    bgColor: '#17212b',
+    buttonColor: '#5288c1',
+    buttonTextColor: '#ffffff',
+    destructiveTextColor: '#ec3942',
+    headerBgColor: '#17212b',
+    hintColor: '#708499',
+    linkColor: '#6ab3f3',
+    secondaryBgColor: '#232e3c',
+    sectionBgColor: '#17212b',
+    sectionHeaderTextColor: '#6ab3f3',
+    subtitleTextColor: '#708499',
+    textColor: '#f5f5f5',
+  },
+  initData: parseInitData(initDataRaw),
+  initDataRaw,
+  version: '7.2',
+  platform: 'tdesktop',
+});
 
 export const useTelegram = () => {
   const tg = window.Telegram.WebApp;
