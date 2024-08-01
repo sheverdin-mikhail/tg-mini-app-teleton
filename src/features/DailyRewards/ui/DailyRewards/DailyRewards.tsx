3c360db5c0a3de37f8b0dea5085bb95a390e3334
Mikhail <sheverdin-mikhail@gmail.com>
2024-08-01 07:17:49 +0400

    deleete daily rewards timer

--- src/features/DailyRewards/ui/DailyRewards/DailyRewards.tsx
+++ src/features/DailyRewards/ui/DailyRewards/DailyRewards.tsx
@@ -1,17 +1,14 @@
 import clsx from 'clsx';
 import { InlineButtons, Title } from '@telegram-apps/telegram-ui';
 import { useSelector } from 'react-redux';
-import { getUserAvailableToClaimDailyRewardDate, getUserCurrentDailyReward } from '@/entities/User';
-import { useEffect, useMemo, useState } from 'react';
+import { getUserCurrentDailyReward } from '@/entities/User';
+import { useMemo  } from 'react';
 import { DailyReward } from '@/entities/DailyReward';
-// import moment from 'moment';
 import cls from './DailyRewards.module.scss';
 import { DailyRewardItem } from '../DailyRewardItem/DailyRewardItem';
 import { useGetDailyRewardsList } from '../../api/dailyRewardsApi';
 import { DailyRewardsSkeleton } from '../DailyRewardsSkeleton/DailyRewardsSkeleton';
-import { TimeProps, Timer } from '@/shared/ui/Timer/Timer';
-import { getDateTime } from '@/shared/utils/getUTCDateTime';
-import moment from 'moment';
+
 
 interface DailyRewardsProps {
     className?: string;
@@ -20,9 +17,6 @@ interface DailyRewardsProps {
 export const DailyRewards: React.FC<DailyRewardsProps> = (props) => {
   const { className } = props;
   const { isError, isLoading, data: dailyRewardsList } = useGetDailyRewardsList();
-  const [time, setTime] = useState<TimeProps>();
-  // const userLastDailyRewardDate = useSelector(getUserLastDailyRewardClaimDate);
-  const userAvailableToClaimDailyRewardDate = useSelector(getUserAvailableToClaimDailyRewardDate);
   const lastDailyReward = useSelector(getUserCurrentDailyReward);
 
 
@@ -44,30 +38,6 @@ export const DailyRewards: React.FC<DailyRewardsProps> = (props) => {
     return lastDailyReward;
   }, [lastDailyReward]);
 
-  useEffect(() => {
-    const getDailyTime = async () => {
-      const now = moment(await getDateTime()) // Текущее время в формате UTC
-      const rewardTime = moment(userAvailableToClaimDailyRewardDate); // Время, когда пользователь сможет получить награду
-
-      const duration = moment.duration(rewardTime.diff(now));
-
-      const hours = Math.floor(duration.asHours());
-      const minutes = duration.minutes();
-      const seconds = duration.seconds();
-
-      console.log(now)
-      console.log(rewardTime)
-
-      setTime({
-        hours,
-        minutes,
-        seconds
-      });
-    }
-
-    getDailyTime();
-  }, [userAvailableToClaimDailyRewardDate]);
-
   if (isError) {
     return <div>Неудалось загрузить ежедневные награды</div>;
   }
@@ -79,9 +49,6 @@ export const DailyRewards: React.FC<DailyRewardsProps> = (props) => {
   return (
     <div className={cls.dailyRewards}>
       <Title caps weight="2" className={cls.title}>Daily Rewards</Title>
-      {
-        time && <Timer className={cls.timer} time={time} />
-      }
       {/* @ts-ignore */}
       <InlineButtons className={clsx(cls.dailyRewardItems, {}, [className])}>
         {
