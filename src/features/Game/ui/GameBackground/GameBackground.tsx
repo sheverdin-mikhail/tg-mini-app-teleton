348f40eea448eda6c68d7bdbcf6532081625abc8
Mikhail <sheverdin-mikhail@gmail.com>
2024-08-05 23:53:17 +0400

    fix scroll on mainPage

--- src/features/Game/ui/GameBackground/GameBackground.tsx
+++ src/features/Game/ui/GameBackground/GameBackground.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import cls from './GameBackground.module.scss';
 
 interface GameBackgroundProps {
@@ -8,14 +8,12 @@ interface GameBackgroundProps {
 export const GameBackground: React.FC<GameBackgroundProps> = (props) => {
   const { level } = props;
   const videoRef = useRef<HTMLVideoElement>(null);
-  const videoSrc = useMemo(() => `/animations/level_${level ?? 1}.mp4`, [level]);
+  const videoSrc = `/animations/level_${level ?? 1}.mp4`;
 
   useEffect(() => {
     if (videoRef.current) {
-      // Изменение src и принудительное обновление видео
-      videoRef.current.src = videoSrc;
-      videoRef.current.load();
       videoRef.current.playbackRate = 2.0;
+      videoRef.current.play();
     }
   }, [videoSrc]);
 
